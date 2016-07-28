9ee6de038e33ba384fa9100b34bb7c90a583bf78
Billy Detschel <billy.detschel@orasi.com>
2016-07-28 15:00:54 -0400

    Requirement #11 as well as variable updates

--- src/main/java/com/lufthansa/lufthansaproj/OffersAndCommPage.java
+++ src/main/java/com/lufthansa/lufthansaproj/OffersAndCommPage.java
@@ -5,10 +5,50 @@
  */
 package com.lufthansa.lufthansaproj;
 
+import java.io.File;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 /**
  *
  * @author colton.porter
  */
 public class OffersAndCommPage {
+    
+    WebDriver driver;
+    File file;
+    By changeSettings = By.xpath("//*[@id='profile-oc']");
+    By yesIConsentToTheProcessingAndUsageOfMyPersonalDataForCommunicationPurposesRadioButton = By.xpath("//*[@id='ns_7_CO19VHUC67E5A0AD0K1N652C92_mmoptions-y']");
+    By saveChangesButton = By.xpath("//*[@id='ns_7_CO19VHUC67E5A0AD0K1N652C92_submit']");
+    By getLatestInformationAboutFarePromotionsAndSpecialOffersByReceivingOurNewsletter = By.xpath("//*[@id='ns_7_CO19VHUC67E5A0AD0K1N652C92_lh-nl']");
+    By getExclusiveOffersForEarningAndRedeemingMilesPeriodRightArrowMore = By.xpath("//*[@id='ns_7_CO19VHUC67E5A0AD0K1N652C92_mam-offer']");
+    
+    public OffersAndCommPage(WebDriver driver) {
+            this.driver = driver;         
+    }
+    
+    public void dataPerm(){
+        
+        //Clicks on the users name link at the top of the page then clicks the profile link within the users link
+        driver.findElement(By.xpath("//*[@id='header-profile-toggle']/i")).click();
+        driver.findElement(By.xpath("//*[@id='uib']/div[1]/div[2]/a")).click();
+        
+        //Clicks on the change settings of the offers and communication section if you can't find it not my problem
+        driver.findElement(changeSettings).click();
+        
+        //Clicks the yes radio button and saves the changes
+        driver.findElement(yesIConsentToTheProcessingAndUsageOfMyPersonalDataForCommunicationPurposesRadioButton).click();
+        driver.findElement(saveChangesButton).click();
+        
+        //Clicks on the change settings of the offers and communication section
+        driver.findElement(changeSettings).click();
+        
+        //Clicks the radio buttons within the e-mail communication section
+        driver.findElement(getLatestInformationAboutFarePromotionsAndSpecialOffersByReceivingOurNewsletter).click();
+        driver.findElement(getExclusiveOffersForEarningAndRedeemingMilesPeriodRightArrowMore).click();
+        
+        driver.findElement(saveChangesButton).click();
+        
+    }
     
 }
--- src/test/java/com/lufthansa/lufthansaproj/OffersAndCommTest.java
+++ src/test/java/com/lufthansa/lufthansaproj/OffersAndCommTest.java
@@ -5,6 +5,11 @@
  */
 package com.lufthansa.lufthansaproj;
 
+import java.io.File;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import static org.testng.Assert.*;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
@@ -12,6 +17,7 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 /**
  *
@@ -19,17 +25,30 @@ import org.testng.annotations.BeforeTest;
  */
 public class OffersAndCommTest {
     
-    public OffersAndCommTest() {
+    WebDriver driver;
+    File file;
+    
+    @Test
+    public void hello() {
+    
+        //Logs in user
+        HomePageLogin L3 = new HomePageLogin(driver);
+        L3.login("firstplacewinner", "dustystick");
+        
+        OffersAndCommPage perm = new OffersAndCommPage(driver);
+        perm.dataPerm();
+        Assert.assertTrue(driver.findElement(By.xpath("//*[@id='contentpage']/div/div[1]/div[3]")).isDisplayed(), "Data Protection update successful");
+    
     }
 
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-    // @Test
-    // public void hello() {}
-
     @BeforeTest
-    public static void setUpClass() throws Exception {
+    public void setUpClass() throws Exception {
+        
+        file = new File(this.getClass().getResource("/drivers/chromedriver.exe").getPath());
+        System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
+        driver = new ChromeDriver();
+        driver.get("http://www.lufthansa.com/us/en/Homepage");
+        
     }
 
     @AfterTest
