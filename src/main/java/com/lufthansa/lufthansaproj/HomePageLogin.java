b4926efb647bb064fcaae6e8bf71984ecd939e38
Colton Porter <colton.porter@orasi.com>
2016-07-28 08:40:20 -0400

    Added a login method and drivers folder

--- src/main/java/com/lufthansa/lufthansaproj/HomePageLogin.java
+++ src/main/java/com/lufthansa/lufthansaproj/HomePageLogin.java
@@ -5,12 +5,25 @@
  */
 package com.lufthansa.lufthansaproj;
 
+import java.io.File;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 /**
  *
  * @author colton.porter
  */
 public class HomePageLogin {
     
-    //BigPoops
+    WebDriver driver; 
+    File file;
+    
+    public void openPageChrome(){  
+        file = new File(this.getClass().getResource("/drivers/chromedriver.exe").getPath());
+        System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
+        driver = new ChromeDriver();
+        driver.get("http://www.lufthansa.com/us/en/Homepage");
+    }
+    
     
 }
