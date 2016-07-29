8ccfa24e7822d38c3919f41fd01da5ba5478eddd
Colton Porter <colton.porter@orasi.com>
2016-07-29 14:05:39 -0400

    got rid of some imports not used

--- src/main/java/com/lufthansa/lufthansaproj/HomePageLogin.java
+++ src/main/java/com/lufthansa/lufthansaproj/HomePageLogin.java
@@ -8,7 +8,6 @@ package com.lufthansa.lufthansaproj;
 import java.io.File;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 
 /**
  *
--- src/test/java/com/lufthansa/lufthansaproj/PersonalDataTest.java
+++ src/test/java/com/lufthansa/lufthansaproj/PersonalDataTest.java
@@ -10,9 +10,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import org.openqa.selenium.chrome.ChromeDriver;
 
